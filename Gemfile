b90439a601708cb5357008267c2648014e052d20
Todd Morningstar <techarchist@gmail.com>
2018-03-13 18:49:16 -0400

    Replace rspec gem with rspec-rails
     * Re-initialize test suite
     * Create sample test to yell at you to make tests

--- Gemfile
+++ Gemfile
@@ -36,7 +36,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
 
-  gem "rspec", "~> 3.7.0"
+  gem "rspec-rails", "~> 3.7.2"
 end
 
 group :development do
---
+++ spec/sample_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe "SampleSpec" do
+  describe "lack of tests" do
+    context "scary" do
+      it "needs to have tests" do
+        pending "WRITE SOME TESTS!"
+        fail
+      end
+    end
+  end
+end
